d668b90af8ab2b09d462885bf711e301abd7822d
cwh <cwh.xpx@gmail.com>
2015-11-27 12:50:54 +1100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/locatr/FlickrFechr.java
@@ -0,0 +1,138 @@
+package com.bignerdranch.android.locatr;
+
+import android.location.Location;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cwh on 11/13/15.
+ */
+public class FlickrFechr {
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "e4a1bbd9696d36e307bb6e167a540095";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException{
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                throw new IOException(connection.getResponseMessage() +
+                                      ": with " +
+                                      urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0){
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        }finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException{
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GallaryItem> fetchRecentPhotos(){
+        String url = buildUrl(FETCH_RECENTS_METHOD, null);
+        return downloadGallaryItems(url);
+    }
+
+    public List<GallaryItem> searchPhotos(String query){
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGallaryItems(url);
+    }
+
+    public List<GallaryItem> searchPhotos(Location location){
+        String url = buildUrl(location);
+        return downloadGallaryItems(url);
+    }
+
+    private List<GallaryItem> downloadGallaryItems(String url){
+        List<GallaryItem> items = new ArrayList<>();
+
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        }catch (JSONException je){
+            Log.e(TAG, "Failed to parse JSON", je);
+        } catch (IOException ioe){
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+
+        return items;
+    }
+
+    private String buildUrl(String method, String query){
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+
+        if(method.equals(SEARCH_METHOD)){
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Location location){
+        return ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
+    }
+
+    private void parseItems(List<GallaryItem> items, JSONObject jsonBody)
+        throws IOException, JSONException{
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GallaryItem item = new GallaryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if(!photoJsonObject.has("url_s")){
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
+            items.add(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/locatr/GallaryItem.java
@@ -0,0 +1,58 @@
+package com.bignerdranch.android.locatr;
+
+import android.net.Uri;
+
+/**
+ * Created by cwh on 11/16/15.
+ */
+public class GallaryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String mOwner) {
+        this.mOwner = mOwner;
+    }
+
+    public Uri getPhotoPageUri(){
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+}
